6cd60e05d19d68884e61c27574c932bac9b602f2
ArnaudDelperier <arnaud.delperier@gmail.com>
2022-03-09 12:07:46 +0100

    house img

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,14 @@
 import "./home.scss"
 
+import houseImg from "../../assets/house.jpg"
+
 function Home() {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+        <h1 className="home_title">Auberge des montagnes</h1>
+        <p className="home_description">Un petit coin de paradis au beau milieu des montagnes</p>
+        <img className="house-img" src={houseImg}></img>
+    </div>
   )
 }
 
